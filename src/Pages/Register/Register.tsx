cf979686e117cc84684f62246367b7391ab25b8f
dianna1234 <01234dianna@gmail.com>
2023-06-16 22:41:56 +0400

    add register functionality

--- src/Pages/Register/Register.tsx
+++ src/Pages/Register/Register.tsx
@@ -1,60 +1,92 @@
-import { useState,FormEvent } from "react"
-import { useDispatch } from "react-redux";
-import { register,  User} from "../../features/users-slice";
-import { AppDispatch } from "../../app/store";
+
+import { useState, FormEvent, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { register, User } from "../../features/users-slice";
+import { AppDispatch, RootState } from "../../app/store";
+import { Link } from "react-router-dom";
+import './Register.scss'
 
 
 function Register() {
   const [user, setUser] = useState<User>({
-    userName:'',
-    email:'',
-    password:''
-  })
-
+    userName: "",
+    email: "",
+    password: ""
+  });
+  const [registered, setRegistered] = useState(false);
+  const errorMessage = useSelector((state: RootState) => state.users.errorRegister);
   const dispatch: AppDispatch = useDispatch();
 
-  function registerUser(e: FormEvent<HTMLFormElement>){
+  function registerUser(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    dispatch(register({user}));
+    dispatch(register({ user })).then((res)=>{
+      setRegistered(true);
+    });
+    setUser({ userName: "", email: "", password: "" });
   }
+  useEffect(()=>{
+    const timer = setTimeout(() => {
+      setRegistered(false);
+    }, 3000); 
+    
+    return () => clearTimeout(timer);
+  })
+
   return (
-    <div>
-      <form
-        action=""
-        onSubmit={registerUser}
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          gap: "5px",
-        }}
-      >
-           <input
-          type="text"
-          onChange={(e) =>
-            setUser((prev) => ({ ...prev, userName: e.target.value }))
-          }
-          placeholder="username"
-        />
-        <input
-          type="text"
-          onChange={(e) =>
-            setUser((prev) => ({ ...prev, email: e.target.value }))
-          }
-          placeholder="email"
-        />
-        <input
-          type="password"
-          onChange={(e) =>
-            setUser((prev) => ({ ...prev, password: e.target.value }))
-          }
-          placeholder="password"
-        />
-        <button>register</button>
-      </form>
+    <div className="form-bd">
+      <div className="form-wrapper">
+        <div className="form-container">
+          <h1>Please Register</h1>
+          <form onSubmit={registerUser}>
+            <div className="form-control">
+              <input
+                type="text"
+                onChange={(e) =>
+                  setUser((prev) => ({ ...prev, userName: e.target.value }))
+                }
+                value={user.userName}
+                required
+                placeholder="UserName"
+              />
+            </div>
+            <div className="form-control">
+              <input
+                type="text"
+                onChange={(e) =>
+                  setUser((prev) => ({ ...prev, email: e.target.value }))
+                }
+                value={user.email}
+
+                required
+                placeholder="Email"
+              />
+            </div>
+            <div className="form-control">
+              <input
+                type="password"
+                onChange={(e) =>
+                  setUser((prev) => ({ ...prev, password: e.target.value }))
+                }
+                value={user.password}
+                required
+                placeholder="Password"
+              />
+            </div>
+            {errorMessage && <p className="error-message">{errorMessage.slice(6)}</p>}
+            <button className="login-btn">Register</button>
+            <p className="text">
+              Have an account? <Link to="/login">Login</Link>
+            </p>
+          </form>
+        </div>
+      </div>
+      {registered ? (
+        <div className="registration-success">
+          <p>You have successfully registered!</p>
+        </div>
+      ):null}
     </div>
-  )
+  );
 }
 
-export default Register
+export default Register;
