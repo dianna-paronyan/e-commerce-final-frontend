b2913bbae890ddb8daba477341031d02aa615b71
dianna1234 <01234dianna@gmail.com>
2023-06-16 22:43:53 +0400

    create single Product page

--- src/Pages/SingleProduct/Product.tsx
+++ src/Pages/SingleProduct/Product.tsx
@@ -1,43 +1,74 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { ProductSt, fetchProductById } from '../../features/products-slice';
-import { useParams } from 'react-router-dom';
-import { AppDispatch, RootState } from '../../app/store';
+import { useState } from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProductById, singleProduct } from "../../features/products-slice";
+import { useParams } from "react-router-dom";
+import { AppDispatch } from "../../app/store";
+import "./Product.scss";
+// import Counter from "../../components/Counter";
 
+function Product() {
+  const { id } = useParams<{ id?: string }>();
+  const dispatch: AppDispatch = useDispatch();
+  const product = useSelector(singleProduct);
+  const [currentImage, setCurrentImage] = useState("");
 
-function Product ()  {
-  const { id } = useParams<{id?: string}>();
-  const dispatch:AppDispatch = useDispatch();
-  const product = useSelector((state: RootState) =>
-    state.products.products.find((p: ProductSt) => p.id === Number(id))
-  );
-console.log(product,'product');
   useEffect(() => {
-    dispatch(fetchProductById(id));
+    if (id) {
+      dispatch(fetchProductById(id));
+    }
   }, [dispatch, id]);
+
+  useEffect(() => {
+    if (product  && product?.Images?.length > 0) {
+      setCurrentImage(product?.Images[0].fileName); 
+    }
+  }, [product]);
 
   if (!product) {
     return <div>Loading...</div>;
   }
 
+  const handleImageClick = (fileName: string) => {
+    setCurrentImage(fileName); 
+  };
+
   return (
-    <div>
-      <h1>{product.name}</h1>
-      <p>Price: ${product.price}</p>
-      <p>{product.description}</p>
+    <div className="product-box">
+   
       <div>
-        <h2>Images:</h2>
-        {product.Images.map((image) => (
-          <img
-            key={image.fileName}
-            src={`http://localhost:5000/images/${image.fileName}`}
-            alt={image.fileName}
-            style={{ width: '200px' }}
-          />
-        ))}
+        <div className="slideshow">
+          <div className="big-image">
+            <img
+              src={`http://localhost:5000/images/${currentImage}`}
+              alt={currentImage}
+              style={{ width: "450px", height:"400px" }}
+            />
+          </div>
+          <div className="small-images">
+            {product?.Images?.map((image) => (
+              <img
+                key={image?.fileName}
+                src={`http://localhost:5000/images/${image.fileName}`}
+                alt={image.fileName}
+                style={{ width: "70px", cursor: "pointer" }}
+                onClick={() => handleImageClick(image.fileName)}
+                className={image?.fileName === currentImage ? "selected" : ""}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="txt-box">
+
+      <h2>{product?.name}</h2>
+      <p>{product?.description}</p>
+      <p>Price: ${product?.price}</p>
+      {/* <Counter /> */}
       </div>
     </div>
   );
 }
 
 export default Product;
+
