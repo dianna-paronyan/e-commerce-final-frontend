aeb98af91762c6343daeaa457e96902d98ec3054
dianna1234 <01234dianna@gmail.com>
2023-06-16 22:41:21 +0400

    add login functionality

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -1,52 +1,78 @@
-import { useState,FormEvent } from "react"
-import { useDispatch } from "react-redux";
+import { useState, FormEvent, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { login, LoginPayload } from "../../features/users-slice";
-import { AppDispatch } from "../../app/store";
-
+import { AppDispatch, RootState } from "../../app/store";
+import { Link, useNavigate } from "react-router-dom";
+import useLocalStorage from "../../hooks/useLocalStorage";
+import "./Login.scss";
 
 function Login() {
   const [user, setUser] = useState<LoginPayload>({
-    email:'',
-    password:''
-  })
-
+    email: "",
+    password: "",
+  });
+  const [loggedIn, setLoggedIn] = useState<boolean>(false);
+  const errorMessage = useSelector((state: RootState) => state.users.errorLogin);
+  const navigate = useNavigate();
+  const { userInStorage, decoded } = useLocalStorage();
   const dispatch: AppDispatch = useDispatch();
-
-  function loginUser(e: FormEvent<HTMLFormElement>){
+  function loginUser(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    dispatch(login({user}));
+    dispatch(login({ user })).then(() => {
+      if (userInStorage && decoded) {
+        setLoggedIn(true);
+        navigate(`/cartItem/${decoded.id}`);
+      }
+    })
+    .catch((error) => {
+      console.log(error,'error'); 
+    });
+    setUser({email:'', password:''});
   }
+  useEffect(()=>{
+    if (loggedIn && decoded) {
+        navigate(`/cartItem/${decoded.id}`);
+    }
+  },[loggedIn,decoded])
   return (
-    <div>
-      <form
-        action=""
-        onSubmit={loginUser}
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          gap: "5px",
-        }}
-      >
-        <input
-          type="text"
-          onChange={(e) =>
-            setUser((prev) => ({ ...prev, email: e.target.value }))
-          }
-          placeholder="email"
-        />
-        <input
-          type="password"
-          onChange={(e) =>
-            setUser((prev) => ({ ...prev, password: e.target.value }))
-          }
-          placeholder="password"
-        />
-        <button>login</button>
-      </form>
-    </div>
-  )
+        <div className="form-bd">
+          <div className="form-wrapper">
+            <div className="form-container">
+              <h1> Please Login</h1>
+              <form onSubmit={loginUser}>
+                <div className="form-control">
+                  <input
+                    type="text"
+                    onChange={(e) =>
+                      setUser((prev) => ({ ...prev, email: e.target.value }))
+                    }
+                    value={user.email}
+                    required
+                    placeholder="Email"
+                  />
+                </div>
+
+                <div className="form-control">
+                  <input
+                    type="password"
+                    onChange={(e) =>
+                      setUser((prev) => ({ ...prev, password: e.target.value }))
+                    }
+                    value={user.password}
+                    required
+                    placeholder="Password"
+                  />
+                </div>
+                {errorMessage && <p className="error-message">{errorMessage.slice(6)}</p>}
+                <button className="login-btn">Login</button>
+                <p className="text">
+                  Don't have an account? <Link to='/register'> Register</Link>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+  );
 }
 
-export default Login
+export default Login;
