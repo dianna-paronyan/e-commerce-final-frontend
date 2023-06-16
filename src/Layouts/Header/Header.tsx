6f425eefd2c412372f4fdde9ecb7d0675e8a0573
dianna1234 <01234dianna@gmail.com>
2023-06-16 22:38:00 +0400

    create Header

--- src/Layouts/Header/Header.tsx
+++ src/Layouts/Header/Header.tsx
@@ -1,61 +1,117 @@
-
-import { Link } from "react-router-dom";
-import { decodeToken } from "react-jwt";
-import { useState } from "react";
-import { useSelector } from "react-redux";
-import { getCartItems } from "../../features/cart-slice";
+import { Link, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import useLocalStorage from "../../hooks/useLocalStorage";
+import {
+  fetchCategories,
+  getCategories,
+} from "../../features/categories-slice";
+import { useCartItemCount } from "../../Providers/CartItemCount";
+import { AppDispatch } from "../../app/store";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LocalMallIcon from '@mui/icons-material/LocalMall';
 import "./Header.scss";
 
 function Header() {
-  // interface DecodedT {
-  //   // id:number;
-  //   // email: string;
-  //   // exp: number;
-  //   // iat: number;
-  //   // role: number
-  // }
-  const user = localStorage.getItem("user");
-  const decoded:any  = user && decodeToken(JSON.parse(user)?.jwt);
-  console.log(decoded,'d');
+  const categories = useSelector(getCategories);
+  const { decoded } = useLocalStorage();
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const data = useSelector(getCartItems);
+  const cartId = localStorage.getItem("cartId");
+  const navigate = useNavigate();
+  const dispatch: AppDispatch = useDispatch();
+  const count = useCartItemCount();
 
+ 
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
 
-  return (
-    <div>
-      <header className="header">
-        <div className="logo">Logo</div>
-        <nav className="nav-bar">
-          <a href="#">Home</a>
-          <a href="#">About</a>
-          <a href="#">Products</a>
-          <a href="#">Contact</a>
-        </nav>
+  function logOut() {
+    localStorage.removeItem("user");
+    navigate("/");
+  }
+  function cart() {
+    if (decoded?.id) {
+      navigate(`/cartItem/${decoded?.id}`);
+    } else {
+      navigate("/login");
+    }
+  }
 
-            {user && (
-              <div className="cart-icon">
-                <Link to={`/cartItem/${decoded?.id}`}>
-                  cart
+  useEffect(() => {
+    dispatch(fetchCategories());
+  }, [dispatch]);
+
+  return (
+    <header className="navbar">
+      <div className="logo">
+        <a href="">
+          <img className="logo" src="" alt="" />
+        </a>
+      </div>
+
+      <ul className="nav-links">
+        <input type="checkbox" id="checkbox_toggle" />
+        <label htmlFor="checkbox_toggle" className="hamburger">
+          &#9776;
+        </label>
+
+        <div className="menu">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+
+          <li className="dropdown1">
+            <p>Categories</p>
+            <div className="dropdown-content">
+              {categories?.map((category) => (
+                <Link to={`/category/${category.id}`} key={category.id}>
+                  {category.name}
                 </Link>
-                {data.length}
-              </div>
-            )}
-          <div className="avatar" onClick={toggleDropdown}>
-            <img src="avatar.png" alt="Avatar" />
+              ))}
+            </div>
+          </li>
+
+          <li>
+            <Link to="/#about">About</Link>
+          </li>
+          <li>
+            <Link to="">Contact</Link>
+          </li>
+          <li>
+            <LocalMallIcon className="cart"
+              id="myBtn"
+              onClick={cart}  />
+            {count ? <span className="cart-badge">{count}</span> : null}
+          </li>
+          <li onClick={toggleDropdown} className="avatar">
+          <AccountCircleIcon sx={{fontSize:'30px', cursor:'pointer'}} />
             {isDropdownOpen && (
               <div className="dropdown">
                 <div className="dropdown-item">Profile</div>
-                <div className="dropdown-item">Logout</div>
+                <div className="dropdown-item" onClick={logOut}>
+                  Logout
+                </div>
+                <div
+                  className="dropdown-item"
+                  onClick={() => navigate(`/ordered/${cartId}`)}
+                >
+                  {" "}
+                  Ordered
+                </div>
               </div>
             )}
-          </div>
-      </header>
-    </div>
+          </li>
+          <li>
+            <Link to="/login">Sign In</Link>
+          </li>
+        </div>
+      </ul>
+    </header>
   );
 }
 
 export default Header;
-
