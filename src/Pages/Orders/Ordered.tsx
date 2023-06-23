89f06899f6b101c844d83335789ad186694ddf13
dianna1234 <01234dianna@gmail.com>
2023-06-24 00:12:57 +0400

    get all orders

--- src/Pages/Orders/Ordered.tsx
+++ src/Pages/Orders/Ordered.tsx
@@ -1,41 +1,50 @@
 import { useDispatch, useSelector } from "react-redux";
-import { getOrder, orderById } from "../../features/order-slice";
+import { getOrders, orderById } from "../../features/order-slice";
 import { AppDispatch } from "../../app/store";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import "./Ordered.scss";
 
+
 function Ordered() {
-  const order = useSelector(getOrder);
+  const orders = useSelector(getOrders);
   const dispatch: AppDispatch = useDispatch();
   const { cartId } = useParams<{ cartId?: string }>();
+  console.log(orders,'ord');
+
   useEffect(() => {
-    if (cartId && order?.products?.length) {
+    if (cartId) {
       dispatch(orderById(cartId));
     }
   }, [dispatch, cartId]);
-  console.log(order, "o");
+
   return (
-    <div className="container">
-      <h2>Your Orders</h2>
-      {!order?.products?.length ? (
-        <p className="no-orders">You don't have ordered products</p>
-      ) : (
-        order?.products?.map((product) => (
-          <div className="product-item" key={product.productId}>
-            <img
-              src={`http://localhost:5000/images/${product?.Image?.fileName}`}
-              alt=""
-            />
-            <div className="desc">
-              <p className="name">{product.name}</p>
-              <p className="price">Price {product.price}</p>
-              <p className="quantity">Quantity {product.quantity}</p>
-            </div>
-          </div>
-        ))
-      )}
-    </div>
+<div className="container">
+<>
+        <h2 className="heading">Your Orders</h2>
+        {!orders.length ? (
+          <p className="no-orders">You don't have ordered products</p>
+        ) : (
+          orders.map((order) =>
+            order.products.map((product) => (
+              <div className="product-item" key={product.productId}>
+                <img
+                  src={`http://localhost:5000/images/${product?.Image?.fileName}`}
+                  alt=""
+                />
+                <div className="desc">
+                  <p className="name">{product.name}</p>
+                  <p className="price">Price: {product.price}</p>
+                  <p className="quantity">Quantity: {product.quantity}</p>
+                </div>
+              </div>
+            ))
+          )
+        )}
+      </>
+
+</div>
+
   );
 }
 
