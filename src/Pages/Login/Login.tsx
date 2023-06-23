81503b7c6d731f63b7602cab8952d4e6250cd54c
dianna1234 <01234dianna@gmail.com>
2023-06-24 00:11:06 +0400

    check is verified

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -1,77 +1,102 @@
 import { useState, FormEvent, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { login, LoginPayload } from "../../features/users-slice";
+import { login } from "../../features/users-slice";
 import { AppDispatch, RootState } from "../../app/store";
 import { Link, useNavigate } from "react-router-dom";
-import useLocalStorage from "../../hooks/useLocalStorage";
 import "./Login.scss";
 
 function Login() {
-  const [user, setUser] = useState<LoginPayload>({
+  const [user, setUser] = useState({
     email: "",
     password: "",
   });
-  const [loggedIn, setLoggedIn] = useState<boolean>(false);
-  const errorMessage = useSelector((state: RootState) => state.users.errorLogin);
+  const [verifiedError, setVerifiedError] = useState<string>("");
+  let errorMessage = useSelector((state: RootState) => state.users.errorLogin);
   const navigate = useNavigate();
-  const { userInStorage, decoded } = useLocalStorage();
   const dispatch: AppDispatch = useDispatch();
+
+  const payl = useSelector((state: RootState) => state.users.payl);
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (
+      payl !== null &&
+      payl?.is_verified === 1 &&
+      !errorMessage &&
+      !storedUser
+    ) {
+      localStorage.setItem("user", JSON.stringify(payl));
+      navigate(-1);
+    } else if (payl?.is_verified === 0 && !errorMessage && !storedUser) {
+      setVerifiedError("Confirm Email Verification");
+    }
+  }, [payl, errorMessage, navigate]);
+
   function loginUser(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    dispatch(login({ user })).then(() => {
-      if (userInStorage && decoded) {
-        setLoggedIn(true);
-        navigate(`/cartItem/${decoded.id}`);
-      }
-    })
-    .catch((error) => {
-      console.log(error,'error'); 
-    });
-    setUser({email:'', password:''});
-  }
-  useEffect(()=>{
-    if (loggedIn && decoded) {
-        navigate(`/cartItem/${decoded.id}`);
-    }
-  },[loggedIn,decoded])
-  return (
-        <div className="form-bd">
-          <div className="form-wrapper">
-            <div className="form-container">
-              <h1> Please Login</h1>
-              <form onSubmit={loginUser}>
-                <div className="form-control">
-                  <input
-                    type="text"
-                    onChange={(e) =>
-                      setUser((prev) => ({ ...prev, email: e.target.value }))
-                    }
-                    value={user.email}
-                    required
-                    placeholder="Email"
-                  />
-                </div>
 
-                <div className="form-control">
-                  <input
-                    type="password"
-                    onChange={(e) =>
-                      setUser((prev) => ({ ...prev, password: e.target.value }))
-                    }
-                    value={user.password}
-                    required
-                    placeholder="Password"
-                  />
-                </div>
-                {errorMessage && <p className="error-message">{errorMessage.slice(6)}</p>}
-                <button className="login-btn">Login</button>
-                <p className="text">
-                  Don't have an account? <Link to='/register'> Register</Link>
-                </p>
-              </form>
+    dispatch(login({ user }));
+    setUser({ email: "", password: "" });
+    errorMessage = "";
+  }
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setVerifiedError("");
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [verifiedError]);
+
+  return (
+    <div className="form-bd">
+      <div className="form-wrapper">
+        <div className="form-container">
+          <h1>Log In</h1>
+          <form onSubmit={loginUser}>
+            <div className="form-control">
+              <input
+                type="text"
+                onChange={(e) =>
+                  setUser((prev) => ({ ...prev, email: e.target.value }))
+                }
+                value={user.email}
+                required
+                placeholder="Email"
+              />
             </div>
-          </div>
+
+            <div className="form-control">
+              <input
+                type="password"
+                onChange={(e) =>
+                  setUser((prev) => ({ ...prev, password: e.target.value }))
+                }
+                value={user.password}
+                required
+                placeholder="Password"
+              />
+            </div>
+
+            {errorMessage ? (
+              <p className="error-message">{errorMessage?.slice(6)}</p>
+            ) : (
+              ""
+            )}
+
+            <button className="login-btn">Login</button>
+            <p className="text">
+              Don't have an account? <Link to="/register">Register</Link>
+            </p>
+          </form>
         </div>
+      </div>
+      {verifiedError && (
+        <div className="verify">
+          <p>{verifiedError}</p>
+        </div>
+      )}
+    </div>
   );
 }
 
