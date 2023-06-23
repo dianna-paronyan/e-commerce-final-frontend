f6584d4acfea6934a2e645043f5748f1ac07c7de
dianna1234 <01234dianna@gmail.com>
2023-06-24 00:09:54 +0400

    add is_verified

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -4,7 +4,9 @@ interface Decoded {
   id: number | string | undefined;
   email: string;
   role: number;
+  is_verified:number
 }
+
 function useLocalStorage() {
   const userInStorage = localStorage.getItem("user");
   const decoded: Decoded | null  = userInStorage ? decodeToken(JSON.parse(userInStorage)?.jwt) : null;
