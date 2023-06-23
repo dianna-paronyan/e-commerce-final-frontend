ab5331f652c995fd20bb4b089e6eee03c2f60fec
dianna1234 <01234dianna@gmail.com>
2023-06-24 00:12:12 +0400

    add to cart in category page

--- src/Pages/Categories/Category.tsx
+++ src/Pages/Categories/Category.tsx
@@ -2,16 +2,19 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchCategory, getCategory } from "../../features/categories-slice";
 import { AppDispatch } from "../../app/store";
 import { useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
-import '../../style.scss'
-import './Category.scss'
-
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { createCart } from "../../features/cart-slice";
+import useLocalStorage from "../../hooks/useLocalStorage";
+import "../../style.scss";
+import "./Category.scss";
 
 function Category() {
   const data = useSelector(getCategory);
   const dispatch: AppDispatch = useDispatch();
   const { id } = useParams<{ id?: string }>();
   console.log(data, "categor");
+  const navigate = useNavigate();
+  const { decoded, userInStorage } = useLocalStorage();
 
   useEffect(() => {
     if (id) {
@@ -19,11 +22,29 @@ function Category() {
     }
   }, [dispatch, id]);
 
+  function addToCart(id: number) {
+    if (userInStorage && decoded?.id) {
+      dispatch(createCart({ productId: id, userId: +decoded.id }));
+    } else {
+      navigate("/login");
+    }
+  }
+
   return (
     <div>
-      <div className={data?.name === 'For Women' ? 'back-w-c' : data?.name === 'For Men' ? 'back-m-c' : data?.name === 'Retro' ? 'back-r-c' : ''}></div>
+      <div
+        className={
+          data?.name === "For Women"
+            ? "back-w-c"
+            : data?.name === "For Men"
+            ? "back-m-c"
+            : data?.name === "Retro"
+            ? "back-r-c"
+            : ""
+        }
+      ></div>
 
-      <h2> {data?.name}</h2>
+      <h2 className="c-heading"> {data?.name}</h2>
       <div className="grid">
         {data?.Products?.map((pr) => (
           <article key={pr.id} className="card">
@@ -38,6 +59,7 @@ function Category() {
             <div className="text">
               <h3>{pr.name}</h3>
               <p>{pr.price}AMD</p>
+              <button onClick={() => addToCart(pr.id)}>Add to Cart</button>
             </div>
           </article>
         ))}
